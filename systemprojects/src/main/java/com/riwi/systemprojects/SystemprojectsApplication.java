da7c6d09ce467fb337fba9b677e214944742dd81
andersson vargas <anderssoninedjas@gmail.com>
2026-01-05 13:58:37 -0500

    estructura del proyecto hecha y lista para comenzar a desarrollar

---
+++ systemprojects/src/main/java/com/riwi/systemprojects/SystemprojectsApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.systemprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemprojectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemprojectsApplication.class, args);
+	}
+
+}
